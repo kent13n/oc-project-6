03749cdcb36d23bd156a25709c1ff6e76950e2ff
Quentin Jallet <quentin@netby.io>
2023-07-16 14:56:59 +0200

    Refactoring to use TwentyTwenty theme as parent

--- web/app/themes/planty/functions.php
+++ web/app/themes/planty/functions.php
@@ -7,15 +7,15 @@
  * @package planty
  */
 
-add_action( 'wp_enqueue_scripts', 'twentytwentythree_parent_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'twentytwenty_parent_theme_enqueue_styles' );
 
 /**
  * Enqueue scripts and styles.
  */
-function twentytwentythree_parent_theme_enqueue_styles() {
-	wp_enqueue_style( 'twentytwentythree-style', get_template_directory_uri() . '/style.css' );
+function twentytwenty_parent_theme_enqueue_styles() {
+	wp_enqueue_style( 'twentytwenty-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'planty-style',
 		get_stylesheet_directory_uri() . '/style.css',
-		[ 'twentytwentythree-style' ]
+		[ 'twentytwenty-style' ]
 	);
 }
