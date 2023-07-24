34be568d7a31d0ab5353018b048a45ba8c6cd2cb
Quentin Jallet <quentin@netby.io>
2023-07-24 18:07:27 +0200

    Implement fix: Check for active Memcached plugin before usage

--- web/app/object-cache.php
+++ web/app/object-cache.php
@@ -5,10 +5,14 @@
  */
 
 
+global $wpdb;
 $plugin_dir = (defined('WP_PLUGIN_DIR') ? WP_PLUGIN_DIR : WP_CONTENT_DIR . '/plugins');
+$result = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}options WHERE option_name = 'active_plugins'");
+$plugins = (count($result) > 0) ? unserialize($result[0]->option_value) : null;
+$activation = (isset($_GET['action']) && $_GET['action'] === 'activate' && isset($_GET['plugin']) && $_GET['plugin'] === 'memcached/object-cache.php');
 
 // Then load our object cache plugin (if available).
-if (file_exists($plugin_dir . '/memcached/object-cache.php')) {
+if (file_exists($plugin_dir . '/memcached/object-cache.php') && (in_array('memcached/object-cache.php', $plugins) || $activation)) {
 	require_once $plugin_dir . '/memcached/object-cache.php';
 }
 
@@ -18,7 +22,6 @@ if (file_exists($plugin_dir . '/memcached/object-cache.php')) {
  * (which it assumes since we have this file)
  */
 else {
-
 	// Helper/callback.
 	function set_wp_using_ext_object_cache_to_false()
 	{
